7c0ab2f4988a9e397335a7ea619540985e2bec7e
Vinícius Eira <viniciusantos.eira@gmail.com>
2026-04-09 23:58:33 -0300

    refactor: use Lombok to optimize PerformanceMetricsDTO

--- src/main/java/com/conciliapay/api/core/application/dtos/PerformanceMetricsDTO.java
+++ src/main/java/com/conciliapay/api/core/application/dtos/PerformanceMetricsDTO.java
@@ -1,27 +1,17 @@
 package com.conciliapay.api.core.application.dtos;
 
 import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PerformanceMetricsDTO {
 
     private String month;
     private BigDecimal revenue;
     private BigDecimal expenses;
 
-    public PerformanceMetricsDTO() {}
-
-    public PerformanceMetricsDTO(String month, BigDecimal revenue, BigDecimal expenses) {
-        this.month = month;
-        this.revenue = revenue;
-        this.expenses = expenses;
-    }
-
-    public String getMonth() { return month; }
-    public void setMonth(String month) { this.month = month; }
-
-    public BigDecimal getRevenue() { return revenue; }
-    public void setRevenue(BigDecimal revenue) { this.revenue = revenue; }
-
-    public BigDecimal getExpenses() { return expenses; }
-    public void setExpenses(BigDecimal expenses) { this.expenses = expenses; }
 }
